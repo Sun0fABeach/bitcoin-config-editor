06a11248f1490ac0592900e7b72c2cf0398a91c3
Markus <github.mf3zq@simplelogin.com>
2025-03-20 16:27:06 +0100

    prerender and inline everything

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import favicon from '@/assets/favicon.png'
+
+	const { children } = $props()
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+{@render children()}
