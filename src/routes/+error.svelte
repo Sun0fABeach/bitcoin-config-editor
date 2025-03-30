7c77db4155eaabe12d8ff30c30e6eea2027e7fe0
Markus <github.mf3zq@simplelogin.com>
2025-03-30 20:43:18 +0200

    add global error handling

---
+++ src/routes/+error.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { page } from '$app/state'
+</script>
+
+<h1>{page.status}: {page.error!.message}</h1>
+
+<style lang="postcss">
+	h1 {
+		padding: 0 2rem;
+	}
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { setContext } from 'svelte'
 	import { MediaQuery } from 'svelte/reactivity'
+	import Button from '@/components/button.svelte'
 	import { breakpoint } from '@/globals'
 
 	import favicon from '@/assets/favicon.png'
@@ -18,4 +19,35 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-{@render children()}
+<svelte:boundary>
+	{@render children()}
+
+	{#snippet failed(error, reset)}
+		<div class="error-container">
+			<h1>Bitcoin config editor just crashed</h1>
+			<code>{error}</code>
+			<div>
+				<span>Sorry about that. Click here and you might recover:</span>
+				<Button onclick={reset}>Go</Button>
+			</div>
+		</div>
+	{/snippet}
+</svelte:boundary>
+
+<style lang="postcss">
+	.error-container {
+		display: flex;
+		flex-direction: column;
+		align-items: flex-start;
+		row-gap: 1rem;
+		padding: 0 2rem;
+
+		> h1 {
+			margin-bottom: 0;
+		}
+
+		> div > :first-child {
+			margin-right: 0.5rem;
+		}
+	}
+</style>
