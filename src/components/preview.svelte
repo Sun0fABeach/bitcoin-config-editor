e0a7e8c5290e23d59cf224b374e83bc0d0376ec6
Markus <github.mf3zq@simplelogin.com>
2025-04-17 13:55:43 +0200

    hide preview on mobile before hydration

--- src/components/preview.svelte
+++ src/components/preview.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { onMount } from 'svelte'
 	import { scale } from 'svelte/transition'
 	import { building } from '$app/environment'
 	import { getOnDesktopContext } from '@/context/onDesktop'
@@ -20,11 +21,16 @@
 		prevOnDesktop = onDesktop.current
 	})
 
+	/* we need to make sure the preview, although getting prerendered, is
+	 * not visible on mobile before hydration is finished */
+	let visibiltyGuard = $state(true)
+	onMount(() => (visibiltyGuard = false))
+
 	const configText = $derived(previewStore.textContent)
 </script>
 
 {#if previewStore.showPreview || onDesktop.current || building}
-	<aside transition:scaleOnMobile>
+	<aside class={{ 'visibility-guard': visibiltyGuard }} transition:scaleOnMobile>
 		<Panel />
 		<code>
 			<textarea readonly value={configText}></textarea>
@@ -42,6 +48,10 @@
 		width: 100%;
 		height: 100%;
 		background-color: var(--color-background);
+
+		&.visibility-guard {
+			display: none;
+		}
 
 		code {
 			flex-grow: 1;
@@ -65,6 +75,10 @@
 		@media (min-width: 1024px) {
 			position: relative;
 			grid-area: preview;
+
+			&.visibility-guard {
+				display: flex;
+			}
 
 			> code {
 				border-left: 1px dashed var(--color-layout-border);
