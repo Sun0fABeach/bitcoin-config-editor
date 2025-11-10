7d5856ca184abd6e8b0be8c800e80f0783ef7a2c
Markus <github.mf3zq@simplelogin.com>
2025-11-10 15:16:04 +0100

    update knots v29.2 config (20251110)

--- src/configs/knots/29.2.ts
+++ src/configs/knots/29.2.ts
@@ -1,5 +1,26 @@
 import generateConfigKnotsV29_1 from '@/configs/knots/29.1'
+import { patchCategoryOptions } from '@/configs/helpers'
 
 export default function () {
-	return generateConfigKnotsV29_1()
+	const categories = generateConfigKnotsV29_1()
+
+	patchCategoryOptions(categories, 'General', (options) => ({
+		...options,
+
+		lowmem: {
+			...options.lowmem,
+			defaultValue: '0',
+		},
+	}))
+
+	patchCategoryOptions(categories, 'Mempool', (options) => ({
+		...options,
+
+		datacarriersize: {
+			...options.datacarriersize,
+			defaultValue: '83',
+		},
+	}))
+
+	return categories
 }
